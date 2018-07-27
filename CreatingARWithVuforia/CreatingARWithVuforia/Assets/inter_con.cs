93d5c2a551d268d9688984baabf489253692f942
Destroy-Youth <luis.herrerae@outlook.com>
2018-07-27 16:12:16 -0500

    Prueba boton activar sonido

---
+++ CreatingARWithVuforia/CreatingARWithVuforia/Assets/inter_con.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inter_con : MonoBehaviour {
+	
+	public AudioClip[] aClips;
+	public AudioSource myAudioSource;
+	string btnNAme; 
+
+	// Use this for initialization
+	void Start () {
+		myAudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began){
+			Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit Hit;
+			if(Physics.Raycast(ray, out Hit)){
+				btnNAme = Hit.transform.name;
+				switch (btnNAme){
+                    case "Btn_Text1":
+					 myAudioSource.clip = aClips[0];
+					 myAudioSource.Play();
+					 break;
+                    default:
+					 break;
+				}
+			}
+		}
+	}
+}
